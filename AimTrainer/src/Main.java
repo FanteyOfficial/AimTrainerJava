820a65a60eaa133cd53712f1af9f8c5328e76c00
FanteyOfficial <lorida1602@gmail.com>
2023-05-08 12:22:11 +0200

    Update

--- AimTrainer/src/Main.java
+++ AimTrainer/src/Main.java
@@ -1,3 +1,10 @@
+/**
+ * Aim Trainer Project
+ * @author Lorenzo D'Aniello
+ * @author Filippo Zini
+ * @version 1.0
+ */
+
 public class Main {
     public static void main(String[] args) {
         MenuWindow w = new MenuWindow();
